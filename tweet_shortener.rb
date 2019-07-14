79f43471abd8282ebb126829489ff39d99e689a0
Brandon James <brandonj2858@gmail.com>
2019-07-14 13:55:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(tweet)
 tweet.split.collect do |word|
   if dictionary.keys.include?(word)
-    word = dictionary[word]
+    word.downcase = dictionary[word]
   else
     word
   end
