d7f7c44cdd1ff1f4bbb1cbe27dfcbf2aa98d342a
Brandon James <brandonj2858@gmail.com>
2019-07-14 14:30:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,11 +38,4 @@ def selective_tweet_shortener(tweet)
   end
 end
 
-def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet.length) > 140
-    word_substituter(tweet)[0..136] + ...
-  else
-    tweet
-  end
-end
 
